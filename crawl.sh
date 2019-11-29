1ff6c312002250c67ebe4124ce715220aaf45ecf
AlessandroMessori <messori.alessandro.98@gmail.com>
2019-11-29 19:26:51 +0100

    updated crawl.sh

--- crawl.sh
+++ crawl.sh
@@ -1,8 +1,10 @@
-rm -rf data/*.csv
-scrapy crawl goal -o data/goal.csv
-scrapy crawl 90Min -o data/90Min.csv
-scrapy crawl teamTalk -o data/teamTalk.csv
-scrapy crawl soccerNews -o data/soccerNews.csv
-scrapy crawl sportsLens -o data/sportsLens.csv
-scrapy crawl oneFootball -o data/oneFootball.csv
+cd /home/pi/Desktop/Football-Scraper
+rm -rf data
+mkdir data
+/home/pi/.local/bin/scrapy crawl goal -o data/goal.csv
+/home/pi/.local/bin/scrapy crawl 90Min -o data/90Min.csv
+/home/pi/.local/bin/scrapy crawl teamTalk -o data/teamTalk.csv
+/home/pi/.local/bin/scrapy crawl soccerNews -o data/soccerNews.csv
+/home/pi/.local/bin/scrapy crawl sportsLens -o data/sportsLens.csv
+/home/pi/.local/bin/scrapy crawl oneFootball -o data/oneFootball.csv
 cat data/*.csv > data/all.csv
