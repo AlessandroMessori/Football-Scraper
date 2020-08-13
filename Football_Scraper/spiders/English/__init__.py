7d3c7ca33362ddf4b0a8e8625c2bca16e50d7122
AlessandroMessori <messori.alessandro.98@gmail.com>
2020-08-13 14:08:08 +0200

    updated .gitignore

---
+++ Football_Scraper/spiders/English/__init__.py
@@ -0,0 +1,7 @@
+from .goal_spider import GoalSpider
+from .soccerNews_spider import SoccerNewsSpider
+from .Min90_spider import Min90Spider
+from .sportsLens_spider import SportLensSpider
+from .teamTalk_spider import TeamTalkSpider
+from .oneFootball_spyder import OneFootballSpider
+
---
+++ Football_Scraper/spiders/Italian/__init__.py
@@ -0,0 +1,4 @@
+from .calcioMercato_spider import CalcioMercatoSpider
+from .calcioWeb_spider import CalcioWebSpider
+from .gazzetta_spider import GazzettaSpider
+from .sportal_spider import SportalSpider
